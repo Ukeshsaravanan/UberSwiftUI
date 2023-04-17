2123201148bf2ca6b6cd64acf38dc37771ae4795
Ukesh Kumar <ukeshsaravanan@gmail.com>
2023-04-17 10:37:21 +0530

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by UKESH KUMAR on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
